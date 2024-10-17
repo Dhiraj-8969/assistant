8a660f72ef4fe266aa8e7da6c2f7383b6df0817c
Dhiraj kumar <150480294+Dhiraj-8969@users.noreply.github.com>
2024-10-17 07:27:38 +0000

    update js file

--- script.js
+++ script.js
@@ -38,27 +38,65 @@ btn.addEventListener("click", () => {
     voice.style.display = "block"
 })
 
-function takeCommand(msg) {
-    btn.style.display = "flex"
-    voice.style.display = "none"
-    if (msg.includes("hello") || msg.includes("hey")) {
-        speak("hello dhiraj,what can i help you?")
-    } else if (msg.includes("who are you")) {
-        speak("i am shifra virtual assistant , created by dhiraj")
-    } else if (msg.includes("open youtube")) {
-        speak('opening youtube...')
-        window.open("https://www.youtube.com/", "_blank")
-    } else if (msg.includes("open google")) {
-        speak('opening google...')
-        window.open("https://www.google.com/", "_blank")
-    } else if (msg.includes("open calculator")) {
-        speak('opening calculator....')
-        window.open("calculator://")
-    } else if (msg.includes("time")) {
-        let time = new Date().toLocaleString(undefined, { hour: "numeric", minute: "numeric" });
-        speak(time)
-    } else {
-        speak(`this is what i found on internet regarding ${msg.replace("shifra","")||msg.replace("shipra","")}`)
-        window.open(`https://www.bing.com/search?pglt=41&q=${msg.replace("shifra","")||msg.replace("shipra","")}`, "_blank")
-    }
+function takeCommand(message){
+    btn.style.display="flex"
+    voice.style.display="none"
+if(message.includes("hello") || message.includes("hey")){
+    speak("hello sir or madam i am shifra ,how can i help you?")
+}
+else if(message.includes("who are you and what you do")){
+    speak(" Thanks for asking, I am a virtual assistant, created by Ahmad Raza and i am a normal virtual assistant, that help you to do basic things like opening youtube , google ,instagram,searching anything and playing song and etc ")
+}
+else if (message.includes("can you remember my name")){
+    speak("yeah, i will try but i have many members so i can try to remember your name")
+}
+else if (message.includes("open youtube")){
+    speak("opening youtube...")
+    window.open("https://www.youtube.com/","_blank")
+}
+else if (message.includes("open google")){
+    speak("opening google...")
+    window.open("https://www.google.com/","_blank")
+}
+else if (message.includes("open facebook")){
+    speak("opening facebook...")
+    window.open("https://www.facebook.com/","_blank")
+}
+else if (message.includes("open instagram")){
+    speak("opening instagram...")
+    window.open("https://www.instagram.com/","_blank")
+}
+else if (message.includes("open calculator")){
+    speak("opening calculator...")
+    window.open("calculator://")
+}
+else if (message.includes("time")){
+    let time=new Date().toLocaleString(undefined,{hour:"numeric",minute:"numeric"})
+    speak(time)
+}
+else if (message.includes("date")){
+    let date=new Date().toLocaleString(undefined,{day:"numeric",month:"short"})
+    speak(date)
+}
+else if (message.includes("play a song")){
+    speak("playing a song...")
+    window.open("https://www.youtube.com/results?search_query=song+hindi","_blank")
+}
+else if (message.includes("open my college website")){
+    speak("opening your college website...")
+    window.open("https://bbsbec.edu.in/","_blank")
+}
+else if (message.includes("open faculty data")){
+    speak("opening your faculty data...")
+    window.open("https://bbsbec.edu.in/department/cse//","_blank")
+}
+else if (message.includes("play my favourite song")){
+    speak("playing your favourite song...")
+    window.open("https://www.youtube.com/watch?v=60ItHLz5WEA","_blank")
+}
+else{
+     let finalText="this is what i found on internet regarding" + message.replace("shipra","") || message.replace("shifra","")
+    speak(finalText)
+  window.open(`https://www.google.com/search?q=${message.replace("shipra","")}`,"_blank")  
+}
 }
